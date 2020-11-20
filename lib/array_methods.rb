07b4dd9e9b92fca5bbe1b94123fd584c90398222
Allison Nolan <allisonknolan@gmail.com>
2020-11-19 19:40:53 -0600

    commit tree a1ee085f02d7f22e445d95266bf75b01b4d37b41

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
-def using_include(array, element)
-
+def using_include(array, element, element2)
+    ["wow", "I", "am", "really", "learning", "arrays!"]
+    .include?("wow", "Tardis")
 end
 
 def using_sort(array)
-
+    ["wow", "I", "am", "really", "learning", "arrays!"]
+    .sort
 end
 
 def using_reverse(array)
-
+    ["wow", "I", "am", "really", "learning", "arrays!"]
+    .reverse
 end
 
 def using_first(array)
-
+    ["wow", "I", "am", "really", "learning", "arrays!"]
+    .first
 end
 
 def using_last(array)
-
+    ["wow", "I", "am", "really", "learning", "arrays!"]
+    .last
 end
 
 def using_size(array)
-
+    ["wow", "I", "am", "really", "learning", "arrays!"]
+    .size
 end
